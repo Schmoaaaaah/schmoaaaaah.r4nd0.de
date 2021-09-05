92691441e0500ebd75fcc379fe83472a9fbd7b0d
Noah <nkempers@outlook.de>
2021-09-05 13:18:26 +0200

    bump node-sass and remove analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,14 +15,4 @@
     </v-theme-provider>
   </v-app>
 </template>
-<script>
-const _mtm = (window._mtm = window._mtm || [])
-_mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' })
-const d = document
-const g = d.createElement('script')
-const s = d.getElementsByTagName('script')[0]
-g.type = 'text/javascript'
-g.async = true
-g.src = 'https://analytic.r4nd0.com/js/container_z0bPWdur.js'
-s.parentNode.insertBefore(g, s)
-</script>
+<script></script>
